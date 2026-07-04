fc2eecf37111755ded45bc9ca6b8f97eb72c8540
Nicholas Yepez <68082179+Nicholas7536@users.noreply.github.com>
2026-07-04 18:45:57 -0500

    Time: 15 ms (84.61%) | Memory: 19.4 MB (90.11%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -6,12 +6,20 @@ class Solution:
         max_island = 0
         
         def dfs(r, c):
-            if r not in range(rows) or c not in range(cols) or (r, c) in visited or grid[r][c] == 0:
-                return 0
+            stack = [(r, c)]
+            grid[r][c] = 0 
+            area = 0
             
-            visited.add((r, c))
+            while stack:
+                cr, cc = stack.pop()
+                area += 1
+                for dr, dc in ((1,0), (-1,0), (0,1), (0,-1)):
+                    nr, nc = cr + dr, cc + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 0 
+                        stack.append((nr, nc))
             
-            return 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
+            return area
         
         for r in range(rows):
             for c in range(cols):
